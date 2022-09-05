23d54522d316c3eb2d41a8de728bbdeccb87084f
Nova Gas <novagas95@gmail.com>
2022-09-05 23:48:40 +0100

    crypto added

--- src/components/crypto/Crypto.jsx
+++ src/components/crypto/Crypto.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './crypto.css'
+import { useState, useEffect } from 'react'
+
 
 const Crypto = () => {
+
+  const [fetch, setFetch] = useState({})
+
+  useEffect(()=> {
+
+   const update = () => {
+    fetch('https://api.coinstats.app/public/v1/coins?skip=0&limit=10')
+    .then((response) => response.jason())
+    .then((response) => console.log(response))
+   }
+   update()
+  },[])
+
+  
+
+
   return (
-    <div>Crypto</div>
+    <div>
+      <div className='container'>
+        <button onClick={() => console.log(fetch)}>Fetch</button>
+      </div>
+    </div>
   )
 }
 
